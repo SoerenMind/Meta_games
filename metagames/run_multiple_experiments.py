91c02942c2f93cfaf79a8feb5af03d99ce75b48e
SoerenMind <soeren.mindermann@gmail.com>
2018-10-09 16:18:08 -0400

    Added error logging; fixed bug with plotting

--- metagames/run_multiple_experiments.py
+++ metagames/run_multiple_experiments.py
@@ -1,83 +1,51 @@
 """Script that lets you run experiments with different sets of
 hyperparameters to record outcomes. Calls play.py with those hyperparams."""
+import logging
+import os
+
 import metagames.play as play
 import numpy as np
 
 
-def run_compare_biases_settings():
-    independent_vars = ['seed', 'biases', 'biases_init', 'layers_wo_bias']
+def run_joint_vs_alt_and_lr():
+    independent_vars = ['joint_optim', 'lr_out', 'seed']    # For graph titles
 
-    seed_opt = ['0', '1', '2']
-    # biases_opt = ['0', '1']
-    biases_init_opt = ['-1', '0', '1', 'normal']
-    layers_wo_bias_opt = [[], ['0'], ['0', '1'], ['1']]
+    # Independent vars
+    seed_opt = ['0', '1', '2', '3', '4']
+    joint_optim_opt = ['True', 'False']
+    lr_out_opt = ['1.', '0.1', '0.01']
 
-    exp_group_name = '2018-10-04 19h: Biases options'
+    # Fixed hyperparams
+    n_outer_opt = '150000'
+    lr_in = '10'
+
+    # File handling
+    exp_group_name = '2018-10-08 18h: JOINT, LR'
     foldername = exp_group_name
-    n_outer_opt = '15000'
-    lr_out = '0.05'
+    start_logger(exp_group_name)
 
-
-    num_runs = np.product([len(opt) for opt in [seed_opt, biases_init_opt, layers_wo_bias_opt]])
+    num_runs = np.product([len(opt) for opt in [seed_opt, lr_out_opt, joint_optim_opt]])
     run_counter = 0
 
-    for layers_wo_bias in layers_wo_bias_opt:
-        # for biases in biases_opt:
-        for biases_init in biases_init_opt:
+    for lr_out in lr_out_opt:
+        for joint_optim in joint_optim_opt:
             for seed in seed_opt:
                 run_counter += 1
                 print('Run %i / %i' %(run_counter, num_runs))
                 run(independent_vars=independent_vars,
                     seed=seed,
-                    layers_wo_bias=layers_wo_bias,
-                    # biases=biases,
-                    biases_init=biases_init,
+                    lr_out=lr_out,
+                    joint_optim=joint_optim,
+
                     exp_group_name=exp_group_name,
                     n_outer_opt=n_outer_opt,
-                    lr_out=lr_out,
-                    foldername=foldername)
-
-
-def run_compare_learning_rates_and_payoffs():
-    # TODO(sorenmind): log time in filenames and log and errors somewhere.
-    independent_vars = ['lr_out', 'lr_in', 'CC_val', 'DD_val', 'seed']
-
-    seed_opt = ['2', '3', '4', '5']
-    lr_out_opt = ['0.1', '0.01']
-    lr_in_opt = ['1', '10']
-    CCDD_val_opt = [('-0.1', '-2.9'),  ('-0.7', '-2.3'), ('-1', '-2'), ('-0.8', '-2.2')]
-
-    exp_group_name = '2018-10-05 19h: 150k, lr_in, CC, DD'
-    foldername = exp_group_name
-    n_outer_opt = '150000'
-    n_inner_opt_range = (0, 3)
-
-    num_runs = np.product([len(opt) for opt in [seed_opt, lr_out_opt, lr_in_opt, CCDD_val_opt]])
-    run_counter = 0
-
-    for CC_val, DD_val in CCDD_val_opt:
-        for lr_out in lr_out_opt:
-            for lr_in in lr_in_opt:
-                for seed in seed_opt:
-                    run_counter += 1
-                    print('Run %i / %i' %(run_counter, num_runs))
-                    run(independent_vars=independent_vars,
-                        seed=seed,
-                        lr_out=lr_out,
-                        lr_in=lr_in,
-                        CC_val=CC_val,
-                        DD_val=DD_val,
-                        exp_group_name=exp_group_name,
-                        n_outer_opt=n_outer_opt,
-                        foldername=foldername,
-                        n_inner_opt_range=n_inner_opt_range)
+                    foldername=foldername,
+                    lr_in=lr_in,
+                    )
 
 
 def run(**kwargs):
-    """Runs play.py with hyperparams given by kwargs.
-    command_instead_of_main sets whether the play.main() function is called or play is run by
-    a new python instance (so if one run crashes the present script continues with the other runs).
-    """
+    """Runs play.py with hyperparams given by kwargs."""
     arglist = []
     for name, value in sorted(kwargs.items()):
         # Handle flags set to True or False
@@ -99,10 +67,19 @@ def run(**kwargs):
     try:
         print('Running play.main with args:', argstring)
         play.main(arglist)
-    except:
+    except Exception as e:
         print('Experiment failed')
+        logging.exception('Experiment failed: ' + argstring)
+
+
+def start_logger(exp_group_name):
+    """For logging when experiment runs fail"""
+    data_directory = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'data'))
+    pathname = os.path.join(data_directory, 'logs')
+    os.makedirs(pathname, exist_ok=True)
+    logging.basicConfig(filename=os.path.join(pathname, exp_group_name) + '.log', level=logging.DEBUG)
 
 
 if __name__=="__main__":
     # run_compare_biases_settings()
-    run_compare_learning_rates_and_payoffs()
+    run_joint_vs_alt_and_lr()
