e0abbd784f33f8aad14bd4c58e588b5c6410e879
SoerenMind <soeren.mindermann@gmail.com>
2018-10-15 16:04:04 -0400

    small changes to play.py and run_multiple_experiment.py

--- metagames/run_multiple_experiments.py
+++ metagames/run_multiple_experiments.py
@@ -7,41 +7,79 @@ import metagames.play as play
 import numpy as np
 
 
-def run_joint_vs_alt_and_lr():
-    independent_vars = ['joint_optim', 'lr_out', 'seed']    # For graph titles
+# def run_joint_vs_alt_and_lr():
+#     independent_vars = ['joint_optim', 'lr_out', 'seed']    # For graph titles
+#
+#     # Independent vars
+#     seed_opt = ['0', '1', '2', '3', '4']
+#     joint_optim_opt = ['True', 'False']
+#     lr_out_opt = ['1.', '0.1', '0.01']
+#
+#     # Fixed hyperparams
+#     n_outer_opt = '150000'
+#     lr_in = '10'
+#
+#     # File handling
+#     exp_group_name = '2018-10-08 18h: JOINT, LR'
+#     foldername = exp_group_name
+#     start_logger(exp_group_name)
+#
+#     num_runs = np.product([len(opt) for opt in [seed_opt, lr_out_opt, joint_optim_opt]])
+#     run_counter = 0
+#
+#     for lr_out in lr_out_opt:
+#         for joint_optim in joint_optim_opt:
+#             for seed in seed_opt:
+#                 run_counter += 1
+#                 print('Run %i / %i' %(run_counter, num_runs))
+#                 run(independent_vars=independent_vars,
+#                     seed=seed,
+#                     lr_out=lr_out,
+#                     joint_optim=joint_optim,
+#
+#                     exp_group_name=exp_group_name,
+#                     n_outer_opt=n_outer_opt,
+#                     foldername=foldername,
+#                     lr_in=lr_in,
+#                     )
+
+
+def run_linear():
+    independent_vars = ['seed', 'lr_out', 'n_outer_opt']    # For graph titles
 
     # Independent vars
     seed_opt = ['0', '1', '2', '3', '4']
-    joint_optim_opt = ['True', 'False']
-    lr_out_opt = ['1.', '0.1', '0.01']
 
     # Fixed hyperparams
     n_outer_opt = '150000'
     lr_in = '10'
+    lr_out = '1'
+    net_type = 'OppAwareLinearNetSubspace'
+    layer_sizes = ('10')
 
     # File handling
-    exp_group_name = '2018-10-08 18h: JOINT, LR'
+    exp_group_name = '2018-10-09 16h: LINEAR'
     foldername = exp_group_name
     start_logger(exp_group_name)
 
-    num_runs = np.product([len(opt) for opt in [seed_opt, lr_out_opt, joint_optim_opt]])
+    num_runs = np.product([len(opt) for opt in [seed_opt]])
     run_counter = 0
 
-    for lr_out in lr_out_opt:
-        for joint_optim in joint_optim_opt:
-            for seed in seed_opt:
-                run_counter += 1
-                print('Run %i / %i' %(run_counter, num_runs))
-                run(independent_vars=independent_vars,
-                    seed=seed,
-                    lr_out=lr_out,
-                    joint_optim=joint_optim,
-
-                    exp_group_name=exp_group_name,
-                    n_outer_opt=n_outer_opt,
-                    foldername=foldername,
-                    lr_in=lr_in,
-                    )
+    for seed in seed_opt:
+        run_counter += 1
+        print('Run %i / %i' %(run_counter, num_runs))
+        run(# Independent vars:
+            independent_vars=independent_vars,
+            seed=seed,
+            # Fixed vars:
+            exp_group_name=exp_group_name,
+            n_outer_opt=n_outer_opt,
+            foldername=foldername,
+            lr_in=lr_in,
+            lr_out=lr_out,
+            net_type=net_type,
+            layer_sizes=layer_sizes
+            )
 
 
 def run(**kwargs):
@@ -82,4 +120,5 @@ def start_logger(exp_group_name):
 
 if __name__=="__main__":
     # run_compare_biases_settings()
-    run_joint_vs_alt_and_lr()
+    # run_joint_vs_alt_and_lr()
+    run_linear()
