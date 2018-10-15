9f5dd8725820833514918801a8359e21f6c66374
Eric Langlois <eric@langlois.xyz>
2018-10-15 16:58:44 -0400

    Added functional.main.run_experiment function

--- metagames/functional/main.py
+++ metagames/functional/main.py
@@ -1,11 +1,19 @@
 """High-level interface to open-source game experiments.
 """
+import functools
+
 import numpy as np
 import torch
 
 from . import agents
 from . import experiment
 from . import losses
+
+EXPERIMENTS = {
+    "self_play_self_aware": functools.partial(experiment.SelfPlayExperiment, self_aware=True),
+    "self_play_self_unaware": functools.partial(experiment.SelfPlayExperiment, self_aware=False),
+}
+
 
 GAMES = {"prisoners_dilemma": np.array([[-1, -3], [0, 2]], dtype=float)}
 
@@ -34,20 +42,71 @@ OPTIMIZERS = {"grad": torch.optim.SGD, "adam": torch.optim.Adam, "lbfgs": torch.
 PARAMETER_INITIALIZERS = {"scaled_normal": experiment.scaled_normal_initializer, "zeros": lambda rand, n: np.zeros(n)}
 
 
+def run_experiment(
+    experiment, game, num_steps, agents_config, default_agent_config=None, agent_seed=None, parameter_seed=None
+):
+    """Run an open source game play experiment.
+
+    Args:
+        experiment: The experiment type. A key of `EXPERIMENTS`.
+        game: The game. A key of `GAMES`.
+        agents_config: A list of agent configuration dictionaries.
+        default_agent_config: The default agent configuration to use when a key is missing
+            from `agents_config`.
+        agents_seed: The seed used to initialize the agent structure.
+        parameter_seed: The seed used to initialize player parameter vectors.
+
+    The agent configuration dictionary has the following keys:
+    AgentConfig:
+        agent: The agent type. A key of `AGENTS`.
+        num_parameters: Size of the parameter vector.
+        num_opponent_parameters: Optional size of the opponent's parameter vector.
+            Defaults to `num_parameters`.
+        initializer: The parameter vector initializer. A key of `PARAMETER_INITIALIZERS`.
+        loss: The loss function. A key of `LOSSES`.
+        learning_rate: The learning rate.
+        optimizer: The optimizer to sue. A key of `OPTIMIZERS`.
+        step_rate: Number of sub-steps per global step. Optional, defaults to 1.
+        num_players: The number of players using this agent.
+        name: An optional name for the agent.
+
+    Returns:
+        A dictionary of the run data.
+    """
+
+    payoff_matrix = GAMES[game]
+    experiment_runner = EXPERIMENTS[experiment](payoff_matrix, dtype=torch.double)
+    player_specifications = prepare_player_specifications(
+        agents_config, default_config=default_agent_config, agent_seed=agent_seed
+    )
+    return experiment_runner.run(player_specifications=player_specifications, num_steps=num_steps, seed=parameter_seed)
+
+
 def prepare_player_specifications(agents_config, default_config=None, agent_seed=None):
     """Create player specifications a list of agent configuration dictionaries.
 
     Args:
-        agents_config: A list of agent configuration dictionaries. Dictionary keys:
-            - agent (AGENTS key)
-            - num_parameters
-            - num_opponent_parameters
-            - initializer (INITIALIZERS key)
-            - loss (LOSSES key)
-            - learning_rate
-            - optimizer (OPTIMIZERS key)
-            - step_rate
-            - num_players
+        agents_config: A list of agent configuration dictionaries.
+        default_config: The default agent configuration to use when a key is missing
+            from `agents_config`.
+        agents_seed: The seed used to initialize the agent structure.
+
+    The agent configuration dictionary has the following keys:
+    AgentConfig:
+        agent: The agent type. A key of `AGENTS`.
+        num_parameters: Size of the parameter vector.
+        num_opponent_parameters: Optional size of the opponent's parameter vector.
+            Defaults to `num_parameters`.
+        initializer: The parameter vector initializer. A key of `PARAMETER_INITIALIZERS`.
+        loss: The loss function. A key of `LOSSES`.
+        learning_rate: The learning rate.
+        optimizer: The optimizer to sue. A key of `OPTIMIZERS`.
+        step_rate: Number of sub-steps per global step. Optional, defaults to 1.
+        num_players: The number of players using this agent.
+        name: An optional name for the agent.
+
+    Returns:
+        A list of `PlayerSpecification` objects.
     """
     rand = np.random.RandomState(agent_seed)
 
